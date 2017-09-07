ad18fcadb7832c8069d8377a5c5aa631181f79c8
Hoang Van Quang <quanghv@gosol.com.vn>
2017-09-07 18:50:23 +0700

    notification received

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Expo, { Permissions, Notifications } from "expo";
+import { Notifications, Asset } from "expo";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Root, StyleProvider } from "native-base";
@@ -8,6 +8,7 @@ import { Root, StyleProvider } from "native-base";
 import getTheme from "../native-base-theme/components";
 import theme from "../native-base-theme/variables/commonColor";
 
+import config from "./config";
 import allReducers from "./reducers";
 import MainStack from "./containers/Router";
 
@@ -15,6 +16,7 @@ const store = createStore(allReducers, applyMiddleware(thunk));
 
 export default class Main extends React.Component {
   componentWillMount() {
+    this._cacheResourcesAsync();
     // Handle notifications that are received or selected while the app
     // is open. If the app was closed and then opened by tapping the
     // notification (rather than just tapping the app icon to open it),
@@ -29,6 +31,17 @@ export default class Main extends React.Component {
     console.log(notification, "notification");
     // this.setState({ notification });
   };
+
+  async _cacheResourcesAsync() {
+    const images = [config.images.logo];
+
+    for (const image of images) {
+      await Asset.fromModule(image).downloadAsync();
+    }
+
+    this.setState({ isReady: true });
+  }
+
   render() {
     return (
       <Provider store={store}>
